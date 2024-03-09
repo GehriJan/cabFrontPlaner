2fa415694bd0d24d0c7f68e3951d433bc50c7a6f
Jannis Gehring <jannis-gehring@t-online.de>
2024-03-09 02:31:11 +0100

    feat: add rings

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ class bump():
         self.bulbiness = bulbiness
     def function(self, x, y):
         return self.height*np.exp((-1/self.bulbiness)*(np.square(x-self.posX)+np.square(y-self.posY)))
+    
+class rings():
+    def __init__(self, posX, posY, height, bulbiness) -> None:
+        self.posX = posX
+        self.posY = posY
+        self.height = height
+        self.bulbiness = bulbiness/np.power(10,5)
+    def function(self, x, y):
+        return self.height*np.sin(self.bulbiness*np.sqrt(((np.square(x-self.posX)+np.square(y-self.posY)))))
+
 
 class graph():
     
@@ -27,17 +37,17 @@ class graph():
         X, Y = np.meshgrid(x, y)
         
         first = True
-        for bump in self.textures:
+        for characteristic in self.textures:
             if first == True:
-                Z = bump.function(X, Y)
+                Z = characteristic.function(X, Y)
                 first = False
             else:
-                Z += bump.function(X, Y)
+                Z += characteristic.function(X, Y)
         
         scene = dict(
-            xaxis = dict(title='Länge', range = [0,max({self.lengthX, self.lengthY})]),
-            yaxis = dict(title='Breite', range = [0,max({self.lengthX, self.lengthY})]),
-            zaxis = dict(title='Tiefe', range = [0,max({self.lengthX, self.lengthY})]))
+            xaxis = dict(title='Breite (x)', range = [0,max({self.lengthX, self.lengthY})]),
+            yaxis = dict(title='Länge (y)', range = [0,max({self.lengthX, self.lengthY})]),
+            zaxis = dict(title='Tiefe (z)', range = [-50,max({self.lengthX, self.lengthY})]))
         
         fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)])
         
@@ -49,8 +59,12 @@ class graph():
 
         
 if __name__ == "__main__":
-    front = graph(654, 780, 2)
+    front = graph(654, 780, 10)
     front.textures.add(bump(0.61*front.lengthX, 0.61*front.lengthY, 30, 20000))
     front.textures.add(bump(0.61*0.39*front.lengthX, 0.61*0.39*front.lengthY, 15, 20000))
-    front.textures.add(bump(0.4*front.lengthX, 0.8*front.lengthY, 25, 30000))
+    front.textures.add(bump(0.61*0.7*front.lengthX, 0.61*0.7*front.lengthY, -25, 15000))
+    
+    front.textures.add(rings(-1.5*front.lengthX, 0.5*front.lengthY, 5, 5000))
+    front.textures.add(rings(-1.5*front.lengthX, 1.5*front.lengthY, 15, 9000))
+    front.textures.add(rings(2*front.lengthX, 2*front.lengthY, 10, 1000))
     front.plot()
