21e3e350b0c154cf56e4c7ba92c016874db2919c
Jannis Gehring <jannis-gehring@t-online.de>
2024-04-29 17:17:59 +0200

    feat: compressing to specified heights, raising to specified niveau

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ from functions import *
 
 class graph():
     
-    def __init__(self, lengthX, lengthY, stepSize) -> None:
+    def __init__(self, lengthX, lengthY, stepSize, niveaus: tuple) -> None:
         self.lengthX = lengthX
         self.lengthY = lengthY
         self.stepSize = stepSize
+        
+        self.minHeight = niveaus[0]-niveaus[1]
+        self.normalNiveau = niveaus[1]
+        self.maxHeight = niveaus[2]-niveaus[1]
+        
         self.textures = set()
         self.borders = set()
     
@@ -22,16 +27,32 @@ class graph():
         x = np.linspace(0, self.lengthX, int(np.floor(self.lengthX/self.stepSize)))
         y = np.linspace(0, self.lengthY, int(np.floor(self.lengthY/self.stepSize)))
         X, Y = np.meshgrid(x, y)
-        
         Z = 0*X*Y
+        
+        # Sum up all the specified textures
         for i in {-1, 0, 1}:
             for texture in self.textures:
                 cover = texture.cover.function(X,Y+self.lengthY*i) if hasattr(texture, "cover") and texture.cover != None else np.power(X, 0)*np.power(Y, 0)
                 Z += texture.function(X,Y+self.lengthY*i)*cover
-            
+        
+        # Flattening the borders           
         for border in self.borders:
             Z *= border.function(X, Y)
 
+        # Compress to min/max Height
+        minO = np.min(Z)
+        maxO = np.max(Z)
+        minN = self.minHeight
+        maxN = self.maxHeight
+        
+        a = (minN*maxO-maxN*minO)/(minO*minO*maxO-maxO*maxO*minO)
+        b = (maxN*minO*minO-minN*maxO*maxO)/(maxO*minO*minO-minO*maxO*maxO)
+        
+        Z = a*np.square(Z)+b*Z
+                
+        # Raise to normalNiveau        
+        Z += self.normalNiveau
+        
         return Z
     
     def plot(self):
@@ -48,7 +69,8 @@ class graph():
         
         fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)])
         
-        fig.update_layout(title='Schrankfront Modell', scene=scene)
+        title = f"SideTableSurfaceModel\n\tMin:{np.min(Z).round(2)} Max:{np.max(Z).round(2)}"
+        fig.update_layout(title=title, scene=scene)
         fig.show()
 
     def exportAutodesk(self):
@@ -186,19 +208,17 @@ def exportGraph(g: graph):
 if __name__ == "__main__":
     
     # Graph initialisieren
-    front = graph(lengthX=600, lengthY=3.1415926534*460+10+30, stepSize=5)
+    front = graph(lengthX=600, lengthY=3.1415926534*(460+10+30), stepSize=5, niveaus=(20, 30, 50))
     
     
     # Bumps
-    front.textures.add(bump(0.3*front.lengthX, 0.7*front.lengthY, 200, 70, 30))
+    #front.textures.add(bump(350, 1350, 200, 80, 20))
+    #front.textures.add(bump(400, 240, 100, 70, -10))
+    #front.textures.add(bump(560, 240, 80, 50, 15))
     
-    
-    
-    
-    
-    
-    
-    
+    #front.textures.add(bump(100, 0.45*front.lengthY, 40, 60, -15))
+    #front.textures.add(bump(100, 0.55*front.lengthY, 30, 55, 15))
+    '''
     print("bumpList = [")
     for i in range(20):
 
@@ -211,13 +231,23 @@ if __name__ == "__main__":
         front.textures.add(bump(posX, posY, factorX, factorY, height))
         print(f"\tbump({posX}, {posY}, {factorX}, {factorY}, {height}),")
     print("]")
-      
+    '''
     # Rings
-    cover1 = bump(0.3*front.lengthX, front.lengthY, 80, 200, 1)
-    front.textures.add(rings(0.5*front.lengthX, 0.5*front.lengthY, 100, 100, 5, cover1))
-    
+    cover1 = bump(0.7*front.lengthX, 0.5*front.lengthY, 100, 370, 1)
+    front.textures.add(rings(0.3*front.lengthX, 0.5*front.lengthY, 100, 220, 20, cover1))
+    cover2 = bump(0.3*front.lengthX, 0*front.lengthY, 100, 370, 1)
+    front.textures.add(rings(0.7*front.lengthX, 0*front.lengthY, 100, 220, 20, cover2))
+    #cover2 = bump(150, 250, 120, 120, 1)
+    #front.textures.add(rings(150, 250, 70, 70, 13, cover2))
+    #cover3 = bump(100,1500,50, 50, 1)
+    #front.textures.add(rings(100,1500,40,40,10, cover3))
+
+    #cover5 = bump()
+    #front.textures.add(rings(350, 1350, ))
     # Bumpgrids
-    
+    #cover4 = bump(430,1530,100, 100, 1)
+    #front.textures.add(bumpGrid(430, 1530, 100, 100, 20, cover4))
+        
     
     # Kanten
     front.borders.add(smoothEdges("-x", 30, 0.25, 1, front.lengthX, front.lengthY))
